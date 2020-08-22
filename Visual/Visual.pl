9ca11cbdfd538a7a8c66b018de89092f306ed595
Sai Raghavendra Maddhuri <45467402+SaiRagh4@users.noreply.github.com>
2020-08-22 16:01:09 -0400

    Update Visual.pl
    Updated License description

--- Visual/Visual.pl
+++ Visual/Visual.pl
@@ -1,20 +1,21 @@
 #!/usr/bin/perl
 # Copyright (C) 2018 Sai Raghavendra Maddhuri, Genki Terashi, Daisuke Kihara, and Purdue University.
 # This file is a part of Emap2sec package with -
-# Reference:  Sai Raghavendra Maddhuri Venkata Subramaniya, Genki Terashi, and Daisuke Kihara. Protein Secondary Structure Detection in Intermediate Resolution Cryo-Electron Microscopy Maps Using Deep Learning. In submission (2018).
-#
+# Reference:  Sai Raghavendra Maddhuri Venkata Subramaniya, Genki Terashi, and Daisuke Kihara. Protein Secondary Structure Detection in Intermediate Resolution Cryo-Electron Microscopy Maps Using Deep Learning. Nature Methods (2019).
+## License: GPL v3 for academic use. (For commercial use, please contact us for different licensing.)
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# the Free Software Foundation, version 3.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU General Public License V3 for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU v3.0 General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.en.html.
+
 if(@ARGV!=3){
  print "$0 [output of map2train] [output of Sai's script] [-n or -p]\n";
  print "-p : Show predicted data\n";
