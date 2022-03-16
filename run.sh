c5b0996d9a6e9edbd29b3e3209ef1170b5dbbc5e
Martin <ssalinasmartin@gmail.com>
2022-03-16 13:34:30 +0100

    Updated run.sh to work right right out of the box
    - Creating results folder beforehand avoids not a folder error when copying map2train results
    - Updated paths to match project's folder stucture
    - Added permission to Visual folder to avoid access denied error
    - Changed dataset.py to dataset_wo_stride.py to work without stride file

--- run.sh
+++ run.sh
@@ -19,32 +19,36 @@
 
 
 chmod +x *
-chmod -R 777 ../data
+chmod -R 777 data/
 chmod -R 777 map2train_src/
+chmod -R 777 Visual/
+
+# Create results folder if does not exist
+mkdir -p results
 
 #Inputs
-map='../data/1733.mrc'
+map='data/1733.mrc'
 contour_level=2.5
 
 
 #Vars
-filename='../data/input_file.txt'
-trimmap='../data/trimmap'
-dataset='../data/dataset'
-output='../results/outputP2_0'
-visual_output='../results/visual.pdb'
+filename='data/input_file.txt'
+trimmap='data/trimmap'
+dataset='data/dataset'
+output='results/outputP2_0'
+visual_output='results/visual.pdb'
 
 #Code
-cd ../code/map2train_src
+cd map2train_src
 make
 cd -
-../code/map2train_src/bin/map2train $map -c $contour_level >  $trimmap 
-python dataset.py $trimmap $dataset
+map2train_src/bin/map2train $map -c $contour_level >  $trimmap 
+python data_generate/dataset_wo_stride.py $trimmap $dataset
 echo $dataset > $filename
 echo "INFO : Running Emap2sec.py with arguments $filename"
-python Emap2sec.py "$filename"
+python emap2sec/Emap2sec.py "$filename"
 echo "INFO : Running Visual.pl"
-./Visual.pl "$trimmap" "$output" -p > "$visual_output"
-cp ../code/map2train_src/bin/map2train ../results
+Visual/Visual.pl "$trimmap" "$output" -p > "$visual_output"
+cp map2train_src/bin/map2train results/
 echo "INFO : Done"
 
